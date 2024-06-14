6a3ef9b5d762327eb323d05f61633272c16ba581
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-14 16:30:53 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <div className='font-bold text-blue-600'>
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
