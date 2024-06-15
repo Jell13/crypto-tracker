ab15fae7e63197a02dfd120dbe75f6e8be0b0401
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-15 14:03:30 -0700

    added small features on the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 
 const Navbar = () => {
+
+    const links = [
+        {
+            content: "Home"
+        },
+        {
+            content: "Features"
+        },
+        {
+            content: "Pricing"
+        },
+        {
+            content: "Blog"
+        }
+    ]
   return (
     <div className='text-white flex items-center w-full justify-between lg:px-32 py-6 h-20 border-b-2 border-gray-600'>
       <h1>Logo</h1>
-      <ul className='flex gap-6'>
-        <li>Home</li>
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>Blog</li>
+      <ul className='flex gap-6 cursor-pointer'>
+        {links.map(({content, id})  => (
+            <li className='group hover:scale-110 duration-300 relative'>{content}
+            <span className='absolute group-hover:w-full duration-300 border-b-[2px] left-0 bottom-[0.5px] border-white w-0'>&nbsp;</span></li>
+        ))}
       </ul>
       <div>
         <select name="" id="">
