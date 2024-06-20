93ccd8fca82739a394b5e8a228108b11c23cc405
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-19 17:43:25 -0700

    added a few minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <div className='h-screen w-screen bg-gradient-to-b from-[#0b004e] via-[#1d152f] to-[#002834]'>
+      <div className='min-h-screen w-screen bg-gradient-to-b from-[#0b004e] via-[#1d152f] to-[#002834]'>
         <Navbar/>
       </div>
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,9 +17,9 @@ const Navbar = () => {
         }
     ]
   return (
-    <div className='text-white flex items-center w-full justify-between lg:px-32 py-6 h-20 border-b-2 border-gray-600'>
+    <div className='text-white flex items-center w-full justify-between lg:px-32 py-6 h-20 border-b-2 border-gray-600 fixed'>
       <h1>Logo</h1>
-      <ul className='flex gap-6 cursor-pointer'>
+      <ul className='flex gap-8 cursor-pointer'>
         {links.map(({content, id})  => (
             <li className='group hover:scale-110 duration-300 relative'>{content}
             <span className='absolute group-hover:w-full duration-300 border-b-[2px] left-0 bottom-[0.5px] border-white w-0'>&nbsp;</span></li>
