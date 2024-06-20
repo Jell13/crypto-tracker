f5cff420d41127627f2bee8460587f005cd4b433
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-19 18:06:35 -0700

    added more tiny details in the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,25 @@
 import { MoveUpRight } from 'lucide-react'
 import React from 'react'
+import { useLocation, useNavigation } from 'react-router-dom'
 
 const Navbar = () => {
 
+  const router = useLocation()
     const links = [
         {
+            path: "/",
             content: "Home"
         },
         {
+            path: "/Features",
             content: "Features"
         },
         {
+            path: "/Pricing",
             content: "Pricing"
         },
         {
+            path: "/Blog",
             content: "Blog"
         }
     ]
@@ -21,9 +27,9 @@ const Navbar = () => {
     <div className='text-white flex items-center w-full justify-between lg:px-32 py-6 h-20 border-b-2 border-gray-600 fixed'>
       <h1>CryptoTrack</h1>
       <ul className='flex gap-8 cursor-pointer'>
-        {links.map(({content, id})  => (
-            <li className='group hover:scale-110 duration-300 relative'>{content}
-            <span className='absolute group-hover:w-full duration-300 border-b-[2px] left-0 bottom-[0.5px] border-white w-0'>&nbsp;</span></li>
+        {links.map(({content, id, path})  => (
+            <li key={id} className='group hover:scale-110 duration-300 relative'>{content}
+            <span className={`absolute group-hover:w-full duration-300 border-b-[2px] left-0 bottom-[0.5px] border-white w-0 ${router.pathname === path && "w-full"}`}>&nbsp;</span></li>
         ))}
       </ul>
       <div className='flex gap-4'>
